26a66d900b5da8a975e0b31be0c856477a48c4b6
ElmeddinH <ElmeddinH@users.noreply.github.com>
2026-05-16 15:21:11 +0000

    Task 11: Reload users from file before search

--- Basic_authentication/api/v1/auth/basic_auth.py
+++ Basic_authentication/api/v1/auth/basic_auth.py
@@ -56,6 +56,7 @@ class BasicAuth(Auth):
         if user_pwd is None or not isinstance(user_pwd, str):
             return None
         try:
+            User.load_from_file()
             users = User.search({'email': user_email})
         except Exception:
             return None
