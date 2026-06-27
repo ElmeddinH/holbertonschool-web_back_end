43530bf606f7dca66f67fe2e1b38d2b5ef7fa37c
ElmeddinH <ElmeddinH@users.noreply.github.com>
2026-06-27 18:07:55 +0000

    ES6 Promise Tasks 2-9: then/catch, all, signUp, reject, allSettled, race, try/throw, guardrail

---
+++ ES6_promise/2-then.js
@@ -0,0 +1,12 @@
+export default function handleResponseFromAPI(promise) {
+  return promise
+    .then(() => ({
+      status: 200,
+      body: 'success',
+    }))
+    .catch(() => new Error())
+    .then((result) => {
+      console.log('Got a response from the API');
+      return result;
+    });
+}
